05850fb908b69ada6811aaa867b461fcd123b0a5
kevinslin <kevin@thence.io>
2020-12-16 21:18:44 -0800

    add build script

---
+++ scripts/relink.sh
@@ -0,0 +1,9 @@
+VERSION="0.20.1-alpha.0"
+
+pkg1="@dendronhq/engine-server@$VERSION"
+pkg2="@dendronhq/common-server@$VERSION"
+yarn unlink @dendronhq/engine-server
+yarn unlink @dendronhq/common-server
+echo "installing $pkg"
+yarn add --force $pkg1
+yarn add --force $pkg2
