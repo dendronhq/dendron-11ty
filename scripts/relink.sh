2a096aa26af8a37f6cdf8ae8da83812a44ffadbe
kevinslin <kevin@thence.io>
2020-12-21 12:01:48 -0800

    update deps

--- scripts/relink.sh
+++ scripts/relink.sh
@@ -1,7 +1,8 @@
-VERSION="0.20.1-alpha.13"
+VERSION="0.20.1-alpha.14"
+COMMON_SERVER_VERSION="0.20.1-alpha.13"
 
 pkg1="@dendronhq/engine-server@$VERSION"
-pkg2="@dendronhq/common-server@$VERSION"
+pkg2="@dendronhq/common-server@$COMMON_SERVER_VERSION"
 yarn unlink @dendronhq/engine-server
 yarn unlink @dendronhq/common-server
 echo "installing $pkg"
