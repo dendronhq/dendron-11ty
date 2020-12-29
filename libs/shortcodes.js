e4bcf2edc1922eae231f4ef0bdcc918ff14978b9
kevinslin <kevin@thence.io>
2020-12-29 10:42:58 -0800

    enhance: support nav highlighting from excluded elements

--- libs/shortcodes.js
+++ libs/shortcodes.js
@@ -2,6 +2,7 @@ const remarkRehype = require("remark-rehype");
 const rehypeStringify = require("rehype-stringify");
 const raw = require("rehype-raw");
 const { MDUtilsV4, DendronASTDest } = require("@dendronhq/engine-server");
+const {NoteUtilsV2} = require("@dendronhq/common-all");
 const path = require("path");
 const { DateTime } = require("luxon");
 const {
@@ -11,18 +12,18 @@ const {
   NOTE_UTILS,
   getNavOutput,
   getMetaPath,
-  env
+  env,
 } = require("./utils");
 const fs = require("fs");
 const _ = require("lodash");
-const xmlFiltersPlugin = require('eleventy-xml-plugin')
+const xmlFiltersPlugin = require("eleventy-xml-plugin");
 
 async function formatNote(note) {
   const layout = _.get(note, "custom.layout", false);
   // if (layout === "single") {
   //   return "single"
   // } else {
-    return toMarkdown2(note.body, note.vault, note.fname);
+  return toMarkdown2(note.body, note.vault, note.fname);
   // }
 }
 
@@ -34,8 +35,38 @@ function ms2Date(ts) {
 
 async function markdownfy(contents) {
   const proc = MDUtilsV4.remark();
-  return await MDUtilsV4.procRehype({proc}).process(contents);
+  return await MDUtilsV4.procRehype({ proc }).process(contents);
 }
+
+function getClosetNavVisibleParent(opts) {
+  const { fname, engine, vault } = opts;
+  const notes = engine.notes;
+  const maybeNode = NoteUtilsV2.getNoteByFnameV4({
+    fname,
+    notes,
+    vault,
+  });
+  if (!maybeNode) {
+    throw Error(`no node found for ${fname}, ${vault}`);
+  }
+  let nparent = maybeNode.parent;
+  let permalink = _.get(maybeNode, "custom.permalink", "");
+  if (permalink === "/") {
+    return {id: "root"}
+  } 
+  if (
+    _.some([
+      nparent &&
+        notes[nparent] &&
+        _.get(notes[nparent], "custom.has_collection"),
+    ])
+  ) {
+    return notes[nparent]
+  } else {
+    return maybeNode;
+  }
+}
+
 async function toMarkdown2(contents, vault, fname) {
   const absUrl = NOTE_UTILS.getAbsUrl();
   const config = getDendronConfig();
@@ -44,6 +75,7 @@ async function toMarkdown2(contents, vault, fname) {
   const linkPrefix = absUrl + "/" + siteNotesDir + "/";
   const engine = await getEngine();
   const wikiLinksOpts = { useId: true, prefix: linkPrefix };
+  const navParent = getClosetNavVisibleParent({ fname, vault, engine });
   const proc = MDUtilsV4.procFull({
     engine,
     dest: DendronASTDest.HTML,
@@ -52,14 +84,17 @@ async function toMarkdown2(contents, vault, fname) {
     wikiLinksOpts,
     noteRefOpts: { wikiLinkOpts: wikiLinksOpts, prettyRefs: true },
     publishOpts: {
-      assetsPrefix: (env.stage === "prod") ? sconfig.assetsPrefix : undefined,
-      insertTitle: config.useFMTitle
+      assetsPrefix: env.stage === "prod" ? sconfig.assetsPrefix : undefined,
+      insertTitle: config.useFMTitle,
     },
-    mathOpts: {katex: true}
+    mathOpts: { katex: true },
   });
-  return MDUtilsV4.procRehype({proc, mathjax: true}).process(contents);
+  const navHintElem = `<span id="navId" data="${navParent.id}"></span>`;
+  return (
+    (await MDUtilsV4.procRehype({ proc, mathjax: true }).process(contents)) +
+    navHintElem
+  );
 }
-
 
 let _NAV_CACHE = undefined;
 
@@ -68,10 +103,10 @@ function toNav() {
   if (!fs.existsSync(getMetaPath)) {
     ts = -1;
   } else {
-    ts = fs.readFileSync(getMetaPath(), {encoding: "utf-8"})
+    ts = fs.readFileSync(getMetaPath(), { encoding: "utf-8" });
   }
   if (!_NAV_CACHE || _NAV_CACHE[0] !== ts) {
-    _NAV_CACHE = [ts, fs.readFileSync(getNavOutput(), {encoding: "utf-8"})];
+    _NAV_CACHE = [ts, fs.readFileSync(getNavOutput(), { encoding: "utf-8" })];
   }
   return _NAV_CACHE[1];
 }
@@ -100,7 +135,7 @@ function toToc(note, notesDict) {
 
 function genTemplate(node) {
   let out = [];
-  
+
   /*
 {% if post.header.teaser %}
   {% capture teaser %}{{ post.header.teaser }}{% endcapture %}
@@ -114,71 +149,56 @@ function genTemplate(node) {
   {% assign title = post.title %}
 {% endif %}
 */
-let include = {};
-out.push(`<div class="${include.type || "list"}__item">`);
-out.push(`<article class="archive__item" itemscope itemtype="https://schema.org/CreativeWork">`);
-/*
+  let include = {};
+  out.push(`<div class="${include.type || "list"}__item">`);
+  out.push(
+    `<article class="archive__item" itemscope itemtype="https://schema.org/CreativeWork">`
+  );
+  /*
     {% if include.type == "grid" and teaser %}
       <div class="archive__item-teaser">
         <img src="{{ teaser | relative_url }}" alt="">
       </div>
     {% endif %}
   */
- out.push(`<h2 class="archive__item-title no_toc" itemprop="headline">`);
+  out.push(`<h2 class="archive__item-title no_toc" itemprop="headline">`);
 
-      // {% if post.link %}
-      //   <a href="{{ post.link }}">{{ title }}</a> <a href="{{ post.url | relative_url }}" rel="permalink"><i class="fas fa-link" aria-hidden="true" title="permalink"></i><span class="sr-only">Permalink</span></a>
-      // {% else %}
-    let href = NOTE_UTILS.getAbsUrl(NOTE_UTILS.getUrl(node));
-  out.push(`<a href="${href}" rel="permalink">${ node.title }</a>`);
-      // {% endif %}
+  // {% if post.link %}
+  //   <a href="{{ post.link }}">{{ title }}</a> <a href="{{ post.url | relative_url }}" rel="permalink"><i class="fas fa-link" aria-hidden="true" title="permalink"></i><span class="sr-only">Permalink</span></a>
+  // {% else %}
+  let href = NOTE_UTILS.getAbsUrl(NOTE_UTILS.getUrl(node));
+  out.push(`<a href="${href}" rel="permalink">${node.title}</a>`);
+  // {% endif %}
   out.push(`</h2>`);
-  out.push(`<p class="page__meta"><i class="far fa-clock" aria-hidden="true"></i> ${node.custom.date}</p>`);
+  out.push(
+    `<p class="page__meta"><i class="far fa-clock" aria-hidden="true"></i> ${node.custom.date}</p>`
+  );
   /*
     {% if post.read_time %}
       <p class="page__meta"><i class="far fa-clock" aria-hidden="true"></i> {% include read-time.html %}</p>
     {% endif %}
     */
-   if (_.get(node, "custom.excerpt", false)) {
-    out.push(`<p class="archive__item-excerpt" itemprop="description">${node.custom.excerpt}</p>`);
-   }
+  if (_.get(node, "custom.excerpt", false)) {
+    out.push(
+      `<p class="archive__item-excerpt" itemprop="description">${node.custom.excerpt}</p>`
+    );
+  }
   out.push(`</article></div>`);
   return out.join("\n");
 }
-
 
 function toCollection(note, notesDict) {
   if (note.children.length <= 0) {
     return [];
   }
-  let children = note.children.map(id => notesDict[id])
+  let children = note.children.map((id) => notesDict[id]);
   children = _.sortBy(children, "created");
   if (_.get(note, "custom.sort_order", "normal") === "reverse") {
     children = _.reverse(children);
   }
 
-  return children.map(ch => genTemplate(ch)).join("\n");
-  // if (note.children.length <= 0) {
-  //   return "";
-  // }
-  // const out = [`<hr>`, `<h2 class="text-delta">Table of contents</h2>`, `<ul>`];
-  // // copied from bin/build-nav.js
-  // let notesAtLevel = note.children.map((ent) => notesDict[ent]);
-  // notesAtLevel = _.filter(notesAtLevel, (ent) => {
-  //   return !_.get(ent, "custom.nav_exclude", false);
-  // });
-  // notesAtLevel = _.sortBy(notesAtLevel, ["custom.nav_order", "title"]);
-  // const allLevels = _.map(notesAtLevel, (node) => {
-  //   let level = [`<li>`];
-  //   let href = NOTE_UTILS.getAbsUrl(NOTE_UTILS.getUrl(node));
-
-  //   level.push(`<a href="${href}">${node.title}</a>`);
-  //   level.push(`</li>`);
-  //   return level;
-  // });
-  // return _.flatMap(out.concat(allLevels).concat(["</ul>"])).join("\n");
+  return children.map((ch) => genTemplate(ch)).join("\n");
 }
-
 
 module.exports = {
   configFunction: function (eleventyConfig, options = {}) {
@@ -189,6 +209,6 @@ module.exports = {
     eleventyConfig.addLiquidFilter("ms2Date", ms2Date);
     eleventyConfig.addLiquidFilter("markdownify", markdownfy);
     eleventyConfig.addLiquidFilter("toCollection", toCollection);
-    eleventyConfig.addPlugin(xmlFiltersPlugin)
+    eleventyConfig.addPlugin(xmlFiltersPlugin);
   },
 };
