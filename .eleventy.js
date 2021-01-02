8ba2f63158ee7de66f6c24fdfbf117fa91c493ee
kevinslin <kevin@thence.io>
2021-01-01 23:45:56 -0800

    strip liquid from js

--- .eleventy.js
+++ .eleventy.js
@@ -35,7 +35,7 @@ module.exports = function (eleventyConfig) {
     root: ["_includes"],
     extname: ".liquid",
   });
-  eleventyConfig.addPassthroughCopy(path.join("assets", "js", "vendor"));
+  eleventyConfig.addPassthroughCopy(path.join("assets", "js"));
 
   // --- filters
   eleventyConfig.addLiquidFilter("absolute_url", function (variable) {
